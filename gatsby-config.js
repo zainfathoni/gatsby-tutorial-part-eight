c374073013915b339d3f97c3ad178333241b5f70
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-07 18:06:19 +0800

    Add a manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,18 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#6b37bf',
+        theme_color: '#6b37bf',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png' // This path is relative to the root of the site.
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
